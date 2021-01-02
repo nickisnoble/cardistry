8d6dcd501078afc42272528a48336b19de28eb26
Nick Noble <nick.noble@hey.com>
2021-01-01 22:15:08 -0500

    light refactor of registering suits / kinds

--- lib/cardistry/card.rb
+++ lib/cardistry/card.rb
@@ -11,8 +11,8 @@ module Cardistry
       @kind = kind
       @number = number
 
-      @@suits << @suit unless @@suits.include? @suit
-      @@kinds << @kind unless @@kinds.include? @kind
+      Card.register_suit @suit
+      Card.register_kind @kind
     end
 
     def self.suits
@@ -21,6 +21,17 @@ module Cardistry
 
     def self.kinds
       @@kinds
+    end
+
+
+    private
+
+    def self.register_suit suit
+      @@suits << suit unless @@suits.include? suit
+    end
+
+    def self.register_kind kind
+      @@kinds << kind unless @@kinds.include? kind
     end
   end
 end
